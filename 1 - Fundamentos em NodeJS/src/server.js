4122117499ce66e965b84e243fd2062f7b8d41b5
cauealvesb4@gmail.com <cauealvesb4@gmail.com>
2026-01-29 20:51:34 -0300

    criando rotas com node puro

--- 1 - Fundamentos em NodeJS/src/server.js
+++ 1 - Fundamentos em NodeJS/src/server.js
@@ -1,7 +1,18 @@
 import http from 'node:http';
 
 const server = http.createServer((req, res) => {
-    return res.end('Hello Node!')
+    
+    const { method, url} = req;
+
+    if(method === 'GET' && url === '/users') {
+        return res.end("Buscou usuário");
+    } 
+
+    if(method === 'POST' && url === '/users') {
+        return res.end("Criou Usuários")
+    }
+
+    return res.end("Hello Word")
 });
 
 server.listen(7777);
