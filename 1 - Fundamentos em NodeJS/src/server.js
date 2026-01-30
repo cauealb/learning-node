f862aa7c612b0a0c63c83e93867c91cee87aaf1e
cauealvesb4@gmail.com <cauealvesb4@gmail.com>
2026-01-29 21:10:13 -0300

    setando headers

--- 1 - Fundamentos em NodeJS/src/server.js
+++ 1 - Fundamentos em NodeJS/src/server.js
@@ -1,18 +1,28 @@
 import http from 'node:http';
 
+const users = [];
+
 const server = http.createServer((req, res) => {
-    
     const { method, url} = req;
 
     if(method === 'GET' && url === '/users') {
-        return res.end("Buscou usuário");
+        return res
+            .setHeader('Content-type', 'application/json')
+            .end(JSON.stringify(users))
     } 
 
     if(method === 'POST' && url === '/users') {
-        return res.end("Criou Usuários")
+        users.push({
+            name: 'Cauê',
+            email: 'cauealvesb4@gmail.com'
+        });
+
+        return res
+            .setHeader('Content-type', 'application/json')
+            .end("Usuarios criado com sucesso!");
     }
 
-    return res.end("Hello Word")
+    return res.end("Hello Word") 
 });
 
 server.listen(7777);
