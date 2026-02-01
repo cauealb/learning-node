b2ba8530e373afb11c9f74cb8d5ab9933cf3af68
cauealvesb4@gmail.com <cauealvesb4@gmail.com>
2026-02-01 11:29:14 -0300

    Separando rotas

--- 1 - Fundamentos em NodeJS/src/server.js
+++ 1 - Fundamentos em NodeJS/src/server.js
@@ -1,36 +1,17 @@
 import http from 'node:http';
-import { randomUUID } from 'node:crypto';
 import { json } from './middleware/json.js';
-import { DataBase } from './middleware/database.js';
-
-const database = new DataBase();
+import { routes } from './middleware/routes.js';
 
 const server = http.createServer(async(req, res) => {
     const { method, url} = req;
-
     await json(req, res);
 
-    if(method === 'GET' && url === '/users') {
-        const users = database.select('users');
+    const route = routes.find((route) => {
+        return route.method === method && route.path === url;
+    })
 
-        return res
-            .end(JSON.stringify(users));
-    } 
-
-    if(method === 'POST' && url === '/users') {
-        const { name, email } = req.body;
-
-        const user = {
-            id: randomUUID(),
-            name: name,
-            email: email
-        };
-
-        database.insert('users', user);
-
-        return res
-            .writeHead(201)
-            .end();
+    if (route) {
+        return route.handler(req, res);
     }
 
     return res.writeHead(404).end("Erro na solicitação") 
