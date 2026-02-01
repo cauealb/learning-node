8a59489892b1743fdb551d248af34d18e909a1c0
cauealvesb4@gmail.com <cauealvesb4@gmail.com>
2026-02-01 11:45:47 -0300

    Mudando arquivos

--- 1 - Fundamentos em NodeJS/src/server.js
+++ 1 - Fundamentos em NodeJS/src/server.js
@@ -1,20 +1,20 @@
-import http from 'node:http';
-import { json } from './middleware/json.js';
-import { routes } from './middleware/routes.js';
+import http from "node:http";
+import { json } from "./middleware/json.js";
+import { routes } from "./routes.js";
 
-const server = http.createServer(async(req, res) => {
-    const { method, url} = req;
-    await json(req, res);
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req;
+  await json(req, res);
 
-    const route = routes.find((route) => {
-        return route.method === method && route.path === url;
-    })
+  const route = routes.find((route) => {
+    return route.method === method && route.path === url;
+  });
 
-    if (route) {
-        return route.handler(req, res);
-    }
+  if (route) {
+    return route.handler(req, res);
+  }
 
-    return res.writeHead(404).end("Erro na solicitação") 
+  return res.writeHead(404).end("Erro na solicitação");
 });
 
 server.listen(7777);
