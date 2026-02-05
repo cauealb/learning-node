80a5e0e643dbded314823ef8dde9cba5515e16b0
cauealvesb4@gmail.com <cauealvesb4@gmail.com>
2026-02-05 11:02:23 -0300

    Configurando knex

--- 2 - Criando API REST com NodeJS/src/server.ts
+++ 2 - Criando API REST com NodeJS/src/server.ts
@@ -1,14 +1,17 @@
 import fastify from "fastify";
+import { db } from "./database.js";
+
 const app = fastify();
 
-app.get('/hello', () => {
-  return 'Hello World';
+app.get('/hello', async () => {
+  const result = await db('sqlite_schema').select('*');
+
+  return result
 })
 
-app
-  .listen({
-    port: 3333
-  })
-  .then(() => {
-    console.log('Servidor restartado com sucesso!')
-  })
+app.listen({
+  port: 3333
+
+}).then(() => {
+  console.log('Servidor restartado com sucesso!')
+})
