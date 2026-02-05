af856d4e581e25e39a132997d8ad972ebc90c9e0
cauealvesb4@gmail.com <cauealvesb4@gmail.com>
2026-02-05 13:12:13 -0300

    Criando as migrations

--- 2 - Criando API REST com NodeJS/src/database.ts
+++ 2 - Criando API REST com NodeJS/src/database.ts
@@ -1,8 +1,15 @@
 import knex from 'knex';
 
-export const db = knex.knex({
+export const config: knex.Knex.Config = {
     client: 'sqlite3',
     connection: {
-        filename: './tmp/app.db'
+        filename: './db/app.db'
+    },
+    useNullAsDefault: true,
+    migrations: {
+        extension: 'ts',
+        directory: './db/migrations'
     }
-})
+}
+
+export const db = knex.knex(config)
