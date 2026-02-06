5e1ee8dbac0c2818f45ac84a4567974e0ab68a9e
cauealvesb4@gmail.com <cauealvesb4@gmail.com>
2026-02-06 20:28:22 -0300

    Configurando um hook global

--- 2 - Criando API REST com NodeJS/src/server.ts
+++ 2 - Criando API REST com NodeJS/src/server.ts
@@ -6,8 +6,16 @@ import cookie from '@fastify/cookie';
 const app = fastify();
 
 app.register(cookie);
+app.addHook('preHandler', async () => {
+  console.log(`Olaaaaaaaaa`)
+})
+
 app.register(TransactionRoutes, {
   prefix: 'transaction'
+})
+
+app.get('/hello', async () => {
+  return 'Hello'
 })
 
 app.listen({
