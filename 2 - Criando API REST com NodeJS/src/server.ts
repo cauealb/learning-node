4a4abaa3acae9337bf6bbe018815ec496573dcdd
cauealvesb4@gmail.com <cauealvesb4@gmail.com>
2026-02-08 08:03:52 -0300

    Categorizando os teste

--- 2 - Criando API REST com NodeJS/src/server.ts
+++ 2 - Criando API REST com NodeJS/src/server.ts
@@ -2,7 +2,7 @@ import { app } from "./app.js"
 import { env } from "./env/index.js"
 
 app.listen({
-  port: env.PORT!
+  port: env.PORT
 
 }).then(() => {
   console.log('Servidor restartado com sucesso!')
--- 2 - Criando API REST com NodeJS/src/test/example.spec.ts
+++ 2 - Criando API REST com NodeJS/src/test/example.spec.ts
@@ -1,22 +1,25 @@
-import { test, beforeAll, afterAll } from "vitest";
+import { it, beforeAll, afterAll, describe } from "vitest";
 import request from 'supertest';
 import { app } from "../app.js";
 
-beforeAll(async () => {
-    await app.ready()
+describe('Transaction routes', () => {
+    beforeAll(async () => {
+        await app.ready()
+    })
+
+    afterAll(async () => {
+        await app.close()
+    })
+
+    it('Must be able to create a new  transaction', async () => {
+        await request(app.server)
+            .post('/transaction')
+            .send({
+                title: "Desenvolvedor Full-Stack CLT",
+                amount: 5000,
+                type: "credit"
+            })
+            .expect(201)
+    })
 })
 
-afterAll(async () => {
-    await app.close()
-})
-
-test('The Test create a new transation', async () => {
-    await request(app.server)
-        .post('/transaction')
-        .send({
-            title: "Desenvolvedor Full-Stack CLT",
-            amount: 5000,
-            type: "credit"
-        })
-        .expect(201)
-})
