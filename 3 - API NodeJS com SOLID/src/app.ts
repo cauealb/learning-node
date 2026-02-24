4dfa9522e87cd12a8919615de50a88d76fe30e2f
cauealvesb4@gmail.com <cauealvesb4@gmail.com>
2026-02-24 20:19:40 -0300

    Criação de um usuário

--- 3 - API NodeJS com SOLID/src/app.ts
+++ 3 - API NodeJS com SOLID/src/app.ts
@@ -1,4 +1,26 @@
 import fastify from "fastify";
+import { email, z } from 'zod'
 import { prisma } from "@/lib/prisma.js";
 
 export const app = fastify();
+
+
+app.post('/users',  async (request, replay) => {
+    const requestBodySchema = z.object({
+        name: z.string(),
+        email: z.string().email(),
+        password: z.string().min(7)
+    })
+
+    const { name, email, password } = requestBodySchema.parse(request.body)
+
+    await prisma.user.create({
+        data: {
+            name,
+            email, 
+            password_hash: password
+        }
+    })
+
+    return replay.status(201).send()
+})
