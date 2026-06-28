65c3ead80018e37197ec4c9fee2195026e86c61a
cauealvesb4@gmail.com <cauealvesb4@gmail.com>
2026-06-28 20:37:38 -0300

    feat: Criando teste unitário de validação do email

--- 3 - API NodeJS com SOLID/src/use-case/register.spec.ts
+++ 3 - API NodeJS com SOLID/src/use-case/register.spec.ts
@@ -2,6 +2,7 @@ import { describe, expect, it } from 'vitest'
 import { RegisterUseCase } from './register.js'
 import { compare } from 'bcryptjs'
 import { InMemoryRepository } from '@/repositories/in-memory-repository.js'
+import { EmailAlreadyExistsError } from './errors/email-already-exists-error.js'
 
 describe("Test Unit", () => {
     it("shoul be able create one user", async () => {
@@ -30,5 +31,24 @@ describe("Test Unit", () => {
         const isHashPasswordSuccess = await compare('1234', user.password_hash)
 
         expect(isHashPasswordSuccess).toBe(true);
+    })
+
+    it("should be able to validate email equal", async () => {
+        const UserRepository = new InMemoryRepository()
+        const UseCaseRegister = new RegisterUseCase(UserRepository)
+
+        const email = 'johndoe@gmail.com';
+
+        await UseCaseRegister.execute({
+            name: 'John Doe',
+            email,
+            password: '1234'
+        });
+
+        expect(async () => await UseCaseRegister.execute({
+            name: 'John Doe',
+            email,
+            password: '1234'
+        })).rejects.toBeInstanceOf(EmailAlreadyExistsError)
     })
 })
