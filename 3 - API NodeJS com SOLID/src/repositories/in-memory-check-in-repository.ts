a841074f71d415ea9480b988a11e20eb9c0dab64
cauealvesb4@gmail.com <cauealvesb4@gmail.com>
2026-07-08 10:57:12 -0300

    Criando testes para o checkIn

---
+++ 3 - API NodeJS com SOLID/src/repositories/in-memory-check-in-repository.ts
@@ -0,0 +1,20 @@
+import type { CheckIn } from "prisma/generated/prisma/browser.js";
+import type { CheckInUncheckedCreateInput } from "prisma/generated/prisma/models.js";
+import type { CheckInRepository } from "./prisma/check-in-repository.js";
+
+export class InMemoryCheckInRepository implements CheckInRepository {
+    private items: CheckIn[] = [] 
+
+    async create(data: CheckInUncheckedCreateInput) {
+        const checkIn: CheckIn = {
+            id: 'check-in-01',
+            gym_id: data.gym_id,
+            user_id: data.user_id,
+            created_at: new Date(),
+            validated_at: data.validated_at ? new Date(data.validated_at) : null
+        }
+
+        this.items.push(checkIn)
+        return checkIn
+    }
+}
---
+++ 3 - API NodeJS com SOLID/src/use-case/check-in.spec.ts
@@ -0,0 +1,23 @@
+import type { CheckInRepository } from "@/repositories/prisma/check-in-repository.js";
+import { beforeEach, describe, expect, it } from "vitest";
+import { CheckInUseCase } from "./check-in.js";
+import { InMemoryCheckInRepository } from "@/repositories/in-memory-check-in-repository.js";
+
+let checkInRepository: CheckInRepository
+let sut: CheckInUseCase
+
+describe("Check-In Test", () => {
+    beforeEach(() => {
+        checkInRepository = new InMemoryCheckInRepository()
+        sut = new CheckInUseCase(checkInRepository)
+    })
+
+    it("should be able to create check-in", async () => {
+        const { checkIn } = await sut.execute({
+            userId: 'user-01',
+            gymId: 'gym-01',
+        })
+
+        expect(checkIn.id).toEqual(expect.any(String))
+    })
+})
