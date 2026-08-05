675e93fb6b3095a0557d22c1fbab9631d02e742d
cauealb <cauealvesb4@gmail.com>
2026-08-04 23:40:17 -0300

    >

--- 3 - API NodeJS com SOLID/src/http/controller/gyms/routes.ts
+++ 3 - API NodeJS com SOLID/src/http/controller/gyms/routes.ts
@@ -1,6 +1,14 @@
 import { verifyJWT } from "@/middlewares/verify-jwt.js";
 import type { FastifyInstance } from "fastify";
+import { nearby } from "./nearby.js";
+import { search } from "./search.js";
+import { create } from "./create.js";
 
 export async function gymsRoutes(app: FastifyInstance) {
     app.addHook('onRequest', verifyJWT)
+
+    app.get('/gyms/nearby', nearby)
+    app.get('/gyms/search', search)
+
+    app.post('/gyms', create)
 }
